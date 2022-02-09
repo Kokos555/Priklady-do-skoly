fca19944b02f05ed355d8392c008538b7abf600c
Vít Novotný <novotvit@hotmail.com>
2022-02-09 09:00:21 +0100

    P02

---
+++ P02/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int apomocne = Convert.ToInt32(textBox1.Text);
+            int n = Convert.ToInt32(textBox2.Text);
+            for (int i = 1; i < n; i++)
+            {
+                apomocne *= a;
+            }
+            label1.Text = string.Format("Cisla {0} umocnene na {1} je {2}", a, n, apomocne);
+        }
+    }
+}
